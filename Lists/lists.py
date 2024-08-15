d83569baa0c094b0b48e00967ed15d1a43e61be8
Aiproland <uplimitopenai@gmail.com>
2024-08-15 09:24:35 +0330

    Add files via upload

---
+++ Lists/lists.py
@@ -0,0 +1,27 @@
+phone_numbers = [9121,913,990,991,933]
+
+# print(len(phone_numbers))
+# print(phone_numbers[-3])
+
+# phone_numbers[2] = 998
+# # del phone_numbers[0]
+# # phone_numbers.pop(0)
+# # phone_numbers.remove()
+# # phone_numbers.append(997)
+# phone_numbers.insert(1,993)
+# # phone_numbers.sort(reverse=True)
+# phone_numbers.reverse()
+# print(phone_numbers)
+# for i in phone_numbers:
+#     print(i)
+
+# new_list = phone_numbers[:]
+
+# new_list.append(890)
+# # print(phone_numbers)
+
+# print(phone_numbers[1:])
+National_code=('00151xxx' , '666xxx' , '153xxx')
+# National_code[1] ='123xxx'
+for i in National_code:
+    print(i)
